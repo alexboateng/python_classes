ac503d5f2a02327c26004a898698115c97f8f04e
xelahaippa <43220875+xelahaippa@users.noreply.github.com>
2022-04-06 10:24:29 +0000

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,24 @@
+class KitchenStaff:
+    staff_id=''
+    first_name=''
+    last_name=''
+    role=''
+
+
+class MestStaff:
+    staff_id=''
+    first_name=''
+    last_name=''
+    department=''
+    role=''
+
+
+class DailyMenu:
+    name=''
+    type_of_food=''
+
+
+class Coomodity_for_sale:
+    drinks=''
+    packs=''
+    extra_foods=''
